d728909ff1b56885afbe8223b8d9b19567edd340
DUAL <dual1tyx@hotmail.co.uk>
2021-06-14 20:31:47 +0100

    Move "ChatSettingsData" into ResistanceChatSettings

--- Game/ResistanceChatSettings.c
+++ Game/ResistanceChatSettings.c
@@ -1,4 +1,14 @@
 ref ResistanceChatSettings g_ResistanceChatSettings;
+static ref ResistanceChatSettings GetResistanceChatSettings () {
+    if (!g_ResistanceChatSettings) {
+        g_ResistanceChatSettings = new ref ResistanceChatSettings;
+        
+        if ( GetGame ().IsServer ()){
+            g_ResistanceChatSettings.loadConfig ();
+        }
+    }
+    return g_ResistanceChatSettings;
+}
 
 class ResistanceChatSettings {
 	
@@ -7,69 +17,43 @@ class ResistanceChatSettings {
     protected static string file = "chat-settings.json";
     protected static string fullPath = profilePath + dir + "/" + file;
 
-    protected static ref ChatSettingsData m_ChatSettingsData;
+    static vector defaultColourDirect        = "255 255 255";   // white
+    static vector defaultColourGlobal        = "1 255 238";     // lt blue
+    static vector defaultColourServer        = "255 89 1";      // orange
+    static vector defaultColourAlert         = "255 1 1";       // red
+    static ref array<string> defaultIds      = { "76561197997664497" };
+    static ref RoleSettingsData defaultRole  = new RoleSettingsData("Staff", defaultIds, new SchanaColour(defaultColourAlert));
 
-    static vector defaultColourDirect    = "255 255 255"; // white
-    static vector defaultColourGlobal    = "1 255 238"; // amber 300
-    static vector defaultColourServer    = "255 89 1"; // light blue 300
-    static vector defaultColourAlert     = "255 1 1"; // deep orange 300
+   
 
-    ref SchanaColour ColourDirect        = new SchanaColour (defaultColourDirect);
-    ref SchanaColour ColourDirectPlayer  = new SchanaColour (defaultColourDirect);
-   	ref SchanaColour ColourGlobal        = new SchanaColour (defaultColourGlobal);
-    ref SchanaColour ColourGlobalPlayer  = new SchanaColour (defaultColourGlobal);
-    ref SchanaColour ColourServer        = new SchanaColour (defaultColourServer);
-    ref SchanaColour ColourAlert         = new SchanaColour (defaultColourAlert);
+    protected ref SchanaColour ColourDirect        = new SchanaColour (defaultColourDirect);
+    protected ref SchanaColour ColourDirectPlayer  = new SchanaColour (defaultColourDirect);
+   	protected ref SchanaColour ColourGlobal        = new SchanaColour (defaultColourGlobal);
+    protected ref SchanaColour ColourGlobalPlayer  = new SchanaColour (defaultColourGlobal);
+    protected ref SchanaColour ColourServer        = new SchanaColour (defaultColourServer);
+    protected ref SchanaColour ColourAlert         = new SchanaColour (defaultColourAlert);
+    protected ref array<RoleSettingsData> Roles    = new array<RoleSettingsData>;
 
-    //* Mod Configs
-	
-    // void ResistanceChatSettings(){
-    //     if (!g_ResistanceChatSettings) {
-    //         g_ResistanceChatSettings = new ref ResistanceChatSettings;
-    //     }
-    //     loadConfig();
-    // }
+    //* Mod Configs */
 
-    static ref ResistanceChatSettings ResistanceChatSettings () {
-        if (!g_ResistanceChatSettings) {
-            g_ResistanceChatSettings = new ref ResistanceChatSettings;
-            
-            if ( GetGame ().IsServer ()){
-                g_ResistanceChatSettings.loadConfig();
-            }
-        }
-        return g_ResistanceChatSettings;
-    }
-
-    protected void loadConfig(){
-        if (!FileExist(fullPath)){
-            MakeDirectory(profilePath + dir + "/");
-        }
-
-        m_ChatSettingsData = new ChatSettingsData();
+    void loadConfig(){
         if (FileExist(fullPath)){
-            JsonFileLoader< ChatSettingsData>.JsonLoadFile(profilePath + dir + "/" + file, m_ChatSettingsData);
+            JsonFileLoader<ResistanceChatSettings>.JsonLoadFile(profilePath + dir + "/" + file, this);
         } else {
             makeConfig();
         }
     }
 
     protected void makeConfig(){
-        // ref array<ref string> playerIds;
-        // playerIds.Insert("76561197997664497");
-        // ref array<ref RoleSettingsData> roles = new RoleSettingsData("Staff", playerIds, defaultColourAlert);
-        m_ChatSettingsData = new ChatSettingsData(defaultColourDirect, defaultColourGlobal, defaultColourServer, defaultColourAlert);
-        JsonFileLoader<ChatSettingsData>.JsonSaveFile(profilePath + dir + "/" + file, m_ChatSettingsData);
-    }
-
-    ChatSettingsData getConfig(){
-        if (!m_ChatSettingsData){
-            loadConfig();
+        if (!FileExist(fullPath)){
+            MakeDirectory(profilePath + dir + "/");
         }
-        return m_ChatSettingsData;
+
+        // Roles.Insert(new RoleSettingsData("Staff", defaultIds, ColourAlert));
+        JsonFileLoader<ResistanceChatSettings>.JsonSaveFile(profilePath + dir + "/" + file, this);
     }
 
-    //* Getters
+    //* Getters */
     int GetColourDirect () {
         return ColourDirect.Get ();
     }
@@ -95,15 +79,32 @@ class ResistanceChatSettings {
         return ColourAlert.Get ();
     }
 
+    void SetRoles(){
+         array<string> playerIds = {
+            "76561197997664497"
+        };
+
+        Roles.Insert(new RoleSettingsData("Staff", playerIds, ColourAlert));
+    }
+
     array<RoleSettingsData> GetRoles(){
-        return m_ChatSettingsData.getRoles();
+        return Roles;
+    }
+
+    RoleSettingsData GetRoleByName(string name){
+        for (int i=0; i<Roles.Count();i++){
+            if (Roles[i].GetName() == name){
+                return Roles[i];
+            }
+        }
+        return null;
     }
 
     string GetTest () {
         return "Hello World";
     }
 
-    //* Debug
+    //* Debug */
 
     void Debug (){
 		Print ("[SchanaChat] ColourDirect");
