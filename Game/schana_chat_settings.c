baca2501bb474504b012963cae7dbfad6c2f6556
DUAL <dual1tyx@hotmail.co.uk>
2021-06-14 20:31:55 +0100

    Renamed Getter

--- Game/schana_chat_settings.c
+++ Game/schana_chat_settings.c
@@ -18,7 +18,7 @@ class SchanaModGlobalChatSettings {
 		if (ColorDirect && ColorDirect.R != 0 && ColorDirect.G != 0 && ColorDirect.B != 0){
 			return ColorDirect.Get ();
 		} else {
-			return ResistanceChatSettings ().GetColourDirect ();
+			return GetResistanceChatSettings ().GetColourDirect ();
 		}
     }
 	
@@ -26,7 +26,7 @@ class SchanaModGlobalChatSettings {
 		if (ColorDirectPlayer && ColorDirectPlayer.R != 0 && ColorDirectPlayer.G != 0 && ColorDirectPlayer.B != 0){
 			return ColorDirectPlayer.Get ();
 		} else {
-			return ResistanceChatSettings ().GetColourDirectPlayer ();
+			return GetResistanceChatSettings ().GetColourDirectPlayer ();
 		}
     }
 
@@ -35,7 +35,7 @@ class SchanaModGlobalChatSettings {
 		if (ColorGlobal && ColorGlobal.R != 0 && ColorGlobal.G != 0 && ColorGlobal.B != 0){
 			return ColorGlobal.Get ();
 		} else {
-			return ResistanceChatSettings ().GetColourGlobal ();
+			return GetResistanceChatSettings ().GetColourGlobal ();
 		}
     }
 	
@@ -43,7 +43,7 @@ class SchanaModGlobalChatSettings {
 		if (ColorGlobalPlayer && ColorGlobalPlayer.R != 0 && ColorGlobalPlayer.G != 0 && ColorGlobalPlayer.B != 0){
 			return ColorGlobalPlayer.Get ();
 		} else {
-			return ResistanceChatSettings ().GetColourGlobalPlayer ();
+			return GetResistanceChatSettings ().GetColourGlobalPlayer ();
 		}
     }
 
@@ -51,7 +51,7 @@ class SchanaModGlobalChatSettings {
 		if (ColorServer && ColorServer.R != 0 && ColorServer.G != 0 && ColorServer.B != 0){
 			return ColorServer.Get ();
 		} else {
-			return ResistanceChatSettings ().GetColourServer ();
+			return GetResistanceChatSettings ().GetColourServer ();
 		}
     }
 
@@ -59,7 +59,7 @@ class SchanaModGlobalChatSettings {
 		if (ColorAlert && ColorAlert.R != 0 && ColorAlert.G != 0 && ColorAlert.B != 0){
 			return ColorAlert.Get ();
 		} else {
-			return ResistanceChatSettings ().GetColourAlert ();
+			return GetResistanceChatSettings ().GetColourAlert ();
 		}
     }
 
