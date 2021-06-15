ddc18ab8a82d8c7441b5bd0f318815b480c3cc1f
DUAL <dual1tyx@hotmail.co.uk>
2021-06-15 20:41:59 +0100

    roles, client RPCs for roles send & recieve, role storage etc

--- Mission/mission/missiongameplay.c
+++ Mission/mission/missiongameplay.c
@@ -1,15 +1,18 @@
 modded class MissionGameplay extends MissionBase {
     private bool m_SchanaChatChannelIsGlobal = true;
 	
+    protected ref array<RoleSettingsData> g_ResistanceRoles = new array<RoleSettingsData>;
 	
 	void MissionGameplay (){
         GetRPCManager ().AddRPC ("SchanaChat", "SchanaChatSettingsRPC", this, SingleplayerExecutionType.Both);
+        GetRPCManager ().AddRPC ("SchanaChat", "SchanaRoleColoursRPC", this, SingleplayerExecutionType.Both);
     }
 	
 	override void OnMissionStart (){
 		super.OnMissionStart ();
 		Print ("[SchanaChat] Requesting settings from server");
 		GetRPCManager ().SendRPC ("SchanaChat", "SchanaChatSettingsRPC", new Param1< ResistanceChatSettings >( NULL ), true, NULL);
+        GetRPCManager ().SendRPC ("SchanaChat", "SchanaRoleColoursRPC", new Param1< array<RoleSettingsData> >( NULL ), true, NULL);
 	}
 	
 	void SchanaChatSettingsRPC ( CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target ) {
@@ -19,6 +22,16 @@ modded class MissionGameplay extends MissionBase {
 		Print ("[SchanaChat] Receiving settings from server");
 		
 		g_ResistanceChatSettings = data.param1;
+        Print(g_ResistanceChatSettings);
+	}
+
+    void SchanaRoleColoursRPC (CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target ) {
+        Param1< array<RoleSettingsData> > data  //Player ID, Icon
+		if ( !ctx.Read ( data ) ) return;
+		
+		Print ("[SchanaChat] Receiving roles from server");
+		
+		g_ResistanceRoles = data.param1;
 	}
 
 	
