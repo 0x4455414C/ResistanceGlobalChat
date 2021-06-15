6ec7d2a68605563cb9578edfcd545b5e5da47db8
DUAL <dual1tyx@hotmail.co.uk>
2021-06-15 20:42:19 +0100

    server RPC for role requests, sending role to client and mission

--- Mission/mission/missionserver.c
+++ Mission/mission/missionserver.c
@@ -5,14 +5,23 @@ modded class MissionServer extends MissionBase {
 		// GetSchanaModGlobalChatServerSettings ();
 		GetResistanceChatSettings();
 		GetRPCManager ().AddRPC ( "SchanaChat", "SchanaChatSettingsRPC", this, SingeplayerExecutionType.Both );
+		GetRPCManager ().AddRPC ( "SchanaChat", "SchanaRoleColoursRPC", this, SingeplayerExecutionType.Both );
 	}
 	
 	void SchanaChatSettingsRPC( CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target ) {
 		PlayerIdentity RequestedBy = PlayerIdentity.Cast(sender);
 		if (RequestedBy){
 			Print ("[SchanaChat] Settings Requested By " + RequestedBy.GetId ());
-			GetRPCManager ().SendRPC ("SchanaChat", "SchanaChatSettingsRPC", new Param1< ResistanceChatSettings >( ResistanceChatSettings () ), true, RequestedBy);
+			GetRPCManager ().SendRPC ("SchanaChat", "SchanaChatSettingsRPC", new Param1< ResistanceChatSettings >( GetResistanceChatSettings () ), true, RequestedBy);
 		}
 	}
-	
+
+	void SchanaRoleColoursRPC( CallType type, ref ParamsReadContext ctx, ref PlayerIdentity sender, ref Object target ) {
+		PlayerIdentity RequestedBy = PlayerIdentity.Cast(sender);
+		if (RequestedBy){
+			Print ("[SchanaChat] Roles Requested By " + RequestedBy.GetId ());
+			GetRPCManager ().SendRPC ("SchanaChat", "SchanaRoleColoursRPC", new Param1< array<RoleSettingsData> >( GetResistanceChatSettings ().GetRoles () ), true, RequestedBy);
+		}	
+	}	
+
 }
